fef5eb8eb3d494235be76a636139d979abf4a190
Carlos Pereira Atencio <carlos@microbit.org>
2024-09-05 12:07:54 +0100

    header-gen: Remove node-ts & update cli script.

--- header-gen/src/cli.ts
+++ header-gen/src/cli.ts
@@ -1,7 +1,9 @@
-import { headerData } from "./inputdata";
-import { generateBlob, generateCArray, generateDsHexLiteral } from "./main";
+import process from 'process';
+import { pathToFileURL } from 'url';
+import { headerData } from "./inputdata.js";
+import { generateBlob, generateCArray, generateDsHexLiteral } from "./main.js";
 
-if (require.main === module) {
+if (import.meta.url === pathToFileURL(process.argv[1]).href) {
     const headerBlob = generateBlob(headerData);
     console.log('Blob:', headerBlob);
     console.log(`\nC code:\n${generateCArray(headerBlob)}`);
